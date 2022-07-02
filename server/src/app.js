689acb5248202fdb92521f313c2babdf1af59af6
Igal <vilenskyigal@gmail.com>
2022-07-02 15:33:52 +0200

    express setup

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+console.log("hello");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("combined"));
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/status", (req, res) => {
+  res.send({
+    message: "Hello World",
+  });
+});
+const PORT = "8081";
+app.listen(PORT);
